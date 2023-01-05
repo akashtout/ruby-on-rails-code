458f9a1c374ed3b6c539da0cdd582c2bb96109d2
akashtout <111628726+akashtout@users.noreply.github.com>
2023-01-05 18:32:27 +0530

    Add files via upload

---
+++ ruby on rails.rb
@@ -0,0 +1,10 @@
+step for destroy action with confirmation in rails :-
+ add gem rails-ujs
+a) In application.js
+
+//= require rails-ujs
+
+b) In application.html.erb
+
+ <%= javascript_include_tag "application" %>
+
