70f76a785a9f0465275c4268114fa46fd22a5587
akashtout <111628726+akashtout@users.noreply.github.com>
2023-01-05 18:34:01 +0530

    Update ruby on rails.rb

--- ruby on rails.rb
+++ ruby on rails.rb
@@ -7,4 +7,24 @@ a) In application.js
 b) In application.html.erb
 
  <%= javascript_include_tag "application" %>
+ 
+ Rails search bar  with filter
+
+  def self.search(search, email, name, profile, employee_id)
+    if search.present?
+      result = where("name ILIKE ? or email ILIKE ? or profile ILIKE ? or cast(employee_id as text) ILIKE ? or cast(contact_number as text) ILIKE ?" , "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+
+      if search.present?
+        result.where("email ILIKE ? and name ILIKE ? and profile ILIKE ? and cast(employee_id as text) ILIKE ?", "%#{email}%", "%#{name}%", "%#{profile}%", "%#{employee_id}%")
+      end
+
+    elsif email.present? or name.present? or profile.present? or employee_id.present?
+      where("email ILIKE ? and name ILIKE ? and profile ILIKE ? and cast(employee_id as text) ILIKE ?" ,  "%#{email}%", "%#{name}%", "%#{profile}%", "%#{employee_id}%")
+
+    else search.blank?
+      all
+    end
+  end
+
+
 
